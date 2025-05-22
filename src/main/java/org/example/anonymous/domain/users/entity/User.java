87d84b59fd2a9b1c858c1d196e81389d9c8086ed
pleasebelieveme <cain1227@naver.com>
2025-05-23 07:58:10 +0900

    [feat]: 유저 기본 칼럼 생성

---
+++ src/main/java/org/example/anonymous/domain/users/entity/User.java
@@ -0,0 +1,37 @@
+package org.example.anonymous.domain.users.entity;
+
+import org.example.anonymous.common.base.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private String email;
+
+}
