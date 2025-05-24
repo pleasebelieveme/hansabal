f1a84fbbef9729a0f8a2e2d1cd614467e97b260a
pleasebelieveme <cain1227@naver.com>
2025-05-24 21:15:22 +0900

    [feat]: @EnableJpaAuditing 추가

--- src/main/java/org/example/anonymous/AnonymousApplication.java
+++ src/main/java/org/example/anonymous/AnonymousApplication.java
@@ -2,8 +2,10 @@ package org.example.anonymous;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AnonymousApplication {
 
 	public static void main(String[] args) {
