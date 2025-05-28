cae4c65dd52a0be56000cf31f576696f822d40b9
pleasebelieveme <cain1227@naver.com>
2025-05-28 20:20:40 +0900

    [feat]: Test 이름 변경 AnonymouseApplicationTests -> HansabalApplicationTests

--- src/test/java/org/example/hansabal/HansabalApplicationTests.java
+++ src/test/java/org/example/hansabal/HansabalApplicationTests.java
@@ -1,10 +1,10 @@
-package org.example.anonymous;
+package org.example.hansabal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AnonymousApplicationTests {
+class HansabalApplicationTests {
 
 	@Test
 	void contextLoads() {
