5e7caa306f6df01a2cc68ae567af44afd4b0fc8b
조효준 <148june@naver.com>
2025-05-29 11:05:47 +0900

    fix: BizException

--- src/main/java/org/example/hansabal/domain/product/service/ProductService.java
+++ src/main/java/org/example/hansabal/domain/product/service/ProductService.java
@@ -1,8 +1,10 @@
 package org.example.hansabal.domain.product.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.hansabal.common.exception.BizException;
 import org.example.hansabal.domain.product.dto.request.ProductRequestDto;
 import org.example.hansabal.domain.product.entity.Product;
+import org.example.hansabal.domain.product.exception.ProductErrorCode;
 import org.example.hansabal.domain.product.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 import org.example.hansabal.domain.product.dto.response.ProductResponseDto;
@@ -10,21 +12,23 @@ import org.example.hansabal.domain.product.dto.response.ProductResponseDto;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import static org.example.hansabal.domain.product.exception.ProductErrorCode.INVALID_PRODUCTSTATUS;
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public ProductResponseDto createProduct(ProductRequestDto requestDto) {
-        Product product = Product.of(requestDto.getName(), 10);
+    public ProductResponseDto createProduct(ProductRequestDto request) {
+        Product product = Product.of(request.name(), 10);
         Product savedProduct = productRepository.save(product);
         return new ProductResponseDto(savedProduct.getProductId(), savedProduct.getName());
     }
 
     public ProductResponseDto getProduct(Long id) {
         Product product = productRepository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException("해당 상품이 존재하지 않습니다."));
+                .orElseThrow(() -> new BizException(INVALID_PRODUCTSTATUS));
         return new ProductResponseDto(product.getProductId(), product.getName());
     }
 
@@ -34,10 +38,10 @@ public class ProductService {
                 .toList();
     }
 
-    public ProductResponseDto updateProduct(Long id, ProductRequestDto requestDto) {
+    public ProductResponseDto updateProduct(Long id, ProductRequestDto request) {
         Product product = productRepository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException("해당 상품이 존재하지 않습니다."));
-        product.updateName(requestDto.getName());
+                .orElseThrow(() -> new BizException(INVALID_PRODUCTSTATUS));
+        product.updateName(request.name());
         return new ProductResponseDto(product.getProductId(), product.getName());
     }
 
