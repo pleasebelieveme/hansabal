9c650347ec7693c68641a6a1c036717268e344a1
조효준 <148june@naver.com>
2025-05-30 15:03:06 +0900

    refactor: 실수 수정

--- src/main/java/org/example/hansabal/domain/product/exception/ProductErrorCode.java
+++ src/main/java/org/example/hansabal/domain/product/exception/ProductErrorCode.java
@@ -8,13 +8,13 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ProductErrorCode implements ErrorCode {
-        INVALID_ID(HttpStatus.BAD_REQUEST,"F001" ,"유효하지 않은 아이디입니다."),
-        INVALID_NAME(HttpStatus.BAD_REQUEST,"F002" ,"중복된 이름입니다."),
-        INVALID_PRODUCTSTATUS(HttpStatus.BAD_REQUEST,"F003" ,"해당 상품의 재고가 없습니다."),
-        DUPLICATED_LIST(HttpStatus.BAD_REQUEST,"F004","해당 상품이 장바구니에 이미 존재합니다."),
-        NO_CONTENTS(HttpStatus.NO_CONTENT,"F005","장바구니가 비었습니다."),
-        LOCK_FAILED(HttpStatus.REQUEST_TIMEOUT,"F006","락 시간 초과로 요청 실패"),
-        LOCK_INTERRUPTED(HttpStatus.BAD_REQUEST,"F007","락이 예기치않게 중단되었습니다.");
+        INVALID_ID(HttpStatus.BAD_REQUEST,"P001" ,"유효하지 않은 아이디입니다."),
+        INVALID_NAME(HttpStatus.BAD_REQUEST,"P002" ,"중복된 이름입니다."),
+        INVALID_PRODUCTSTATUS(HttpStatus.BAD_REQUEST,"P003" ,"해당 상품의 재고가 없습니다."),
+        DUPLICATED_LIST(HttpStatus.BAD_REQUEST,"P004","해당 상품이 장바구니에 이미 존재합니다."),
+        NO_CONTENTS(HttpStatus.NO_CONTENT,"P005","장바구니가 비었습니다."),
+        LOCK_FAILED(HttpStatus.REQUEST_TIMEOUT,"P006","락 시간 초과로 요청 실패"),
+        LOCK_INTERRUPTED(HttpStatus.BAD_REQUEST,"P007","락이 예기치않게 중단되었습니다.");
 
 
 
@@ -29,7 +29,7 @@ public enum ProductErrorCode implements ErrorCode {
 
         @Override
         public String getCode() {
-            return this.name();
+            return this.code;
         }
 
         @Override
