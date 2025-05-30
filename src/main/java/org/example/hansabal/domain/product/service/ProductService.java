eb31c9da1a6cc92f6f0e5852247653f130611243
조효준 <148june@naver.com>
2025-05-30 17:48:03 +0900

    refactor: page 수정

--- src/main/java/org/example/hansabal/domain/product/service/ProductService.java
+++ src/main/java/org/example/hansabal/domain/product/service/ProductService.java
@@ -7,10 +7,11 @@ import org.example.hansabal.domain.product.entity.Product;
 import org.example.hansabal.domain.product.exception.ProductErrorCode;
 import org.example.hansabal.domain.product.repository.ProductRepository;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.example.hansabal.domain.product.dto.response.ProductResponseDto;
-
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -19,23 +20,28 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    @Transactional
     public ProductResponseDto createProduct(ProductRequestDto request) {
         Product product = Product.of(request.name(), 10);
         Product savedProduct = productRepository.save(product);
         return ProductResponseDto.from(savedProduct);
     }
 
+    @Transactional(readOnly = true)
     public ProductResponseDto getProduct(Long id) {
         Product product = productRepository.findById(id)
                 .orElseThrow(() -> new BizException(ProductErrorCode.INVALID_PRODUCTSTATUS));
         return ProductResponseDto.from(product);
     }
 
-    public Page<ProductResponseDto> getAllProducts(Pageable pageable) {
-        return productRepository.findAll(pageable)
+    @Transactional(readOnly = true)
+    public Page<ProductResponseDto> getAllProducts(int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        return productRepository.findAll(pageRequest)
                 .map(ProductResponseDto::from);
     }
 
+    @Transactional
     public ProductResponseDto updateProduct(Long id, ProductRequestDto request) {
         Product product = productRepository.findById(id)
                 .orElseThrow(() -> new BizException(ProductErrorCode.INVALID_PRODUCTSTATUS));
@@ -43,6 +49,7 @@ public class ProductService {
         return ProductResponseDto.from(product);
     }
 
+    @Transactional
     public void deleteProduct(Long id) {
         productRepository.deleteById(id);
     }
