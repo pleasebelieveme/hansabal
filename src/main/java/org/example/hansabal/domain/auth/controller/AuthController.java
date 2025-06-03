b081005653ab8b4f81a72ac14481c3201dc54566
pleasebelieveme <cain1227@naver.com>
2025-06-04 08:15:19 +0900

    refactor: 리프레쉬 토큰 접두사 Bearer제거

--- src/main/java/org/example/hansabal/domain/auth/controller/AuthController.java
+++ src/main/java/org/example/hansabal/domain/auth/controller/AuthController.java
@@ -1,16 +1,17 @@
 package org.example.hansabal.domain.auth.controller;
 
 import org.example.hansabal.domain.auth.dto.request.LoginRequest;
+import org.example.hansabal.domain.auth.dto.request.RefreshTokenRequest;
 import org.example.hansabal.domain.auth.dto.response.TokenResponse;
 import org.example.hansabal.domain.auth.service.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -31,5 +32,11 @@ public class AuthController {
 	public ResponseEntity<Void> logout(HttpServletRequest request) {
 		authService.logout(request);
 		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+
+	@PostMapping("/reissue")
+	public ResponseEntity<TokenResponse> reissue(@RequestHeader("Authorization") String refreshToken) {
+		TokenResponse tokenResponse = authService.reissue(refreshToken);
+		return ResponseEntity.ok(tokenResponse);
 	}
 }
