b35ea61f523493adf484ffd5ca2ec0409a29dfe4
조효준 <148june@naver.com>
2025-06-04 19:15:39 +0900

    refactor: 퀴리 수정

--- src/main/java/org/example/hansabal/domain/review/repository/ReviewRepository.java
+++ src/main/java/org/example/hansabal/domain/review/repository/ReviewRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    @Query("SELECT r FROM Review r WHERE r.product.productId = :productId")
+    @Query("SELECT r FROM Review r WHERE r.product.id = :productId")
     List<Review> findAllByProductId(Long productId);
 }
