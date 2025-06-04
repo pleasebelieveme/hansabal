80a2cd0e40210c73e9fc0d224471869fe55c5f94
devgogogogo <imleo322@gmail.com>
2025-06-04 13:51:39 +0900

    feat : 페이징 수정 한번 더 수정했습니다.

--- src/main/java/org/example/hansabal/domain/review/repository/ReviewRepository.java
+++ src/main/java/org/example/hansabal/domain/review/repository/ReviewRepository.java
@@ -1,8 +1,10 @@
 package org.example.hansabal.domain.review.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.example.hansabal.domain.review.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.jpa.repository.Query;
@@ -10,10 +12,8 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    @Query("SELECT r FROM Review r WHERE r.product.productId = :productId")
-    List<Review> findAllByProductId(Long productId);
 
+    @EntityGraph(attributePaths = "product")
     Page<Review> findAllByProductId(Long productId, Pageable pageable);
-
 
 }
