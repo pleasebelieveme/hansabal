b0a94e1c7dafbb9649365ef2a3f83ceab2d26481
pleasebelieveme <cain1227@naver.com>
2025-06-05 10:34:39 +0900

    feat: 유저 O'auth2 구글 적용

--- src/main/java/org/example/hansabal/domain/users/exception/UserErrorCode.java
+++ src/main/java/org/example/hansabal/domain/users/exception/UserErrorCode.java
@@ -15,7 +15,8 @@ public enum UserErrorCode implements ErrorCode {
 	INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "U004",  "유효하지 않은 비밀번호입니다."),
 	NICKNAME_NOT_CHANGED(HttpStatus.BAD_REQUEST, "U005",  "현재 계정의 닉네임과 같습니다."),
 	PASSWORD_NOT_CHANGED(HttpStatus.BAD_REQUEST, "U006",  "현재 계정의 비밀번호와 같습니다."),
-	NO_UPDATE_TARGET(HttpStatus.BAD_REQUEST, "U007",  "닉네임이나 새 비밀번호 중 하나는 반드시 입력되어야 합니다.");
+	NO_UPDATE_TARGET(HttpStatus.BAD_REQUEST, "U007",  "닉네임이나 새 비밀번호 중 하나는 반드시 입력되어야 합니다."),
+	DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST, "U008",  "이미 존재하는 닉네임입니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
