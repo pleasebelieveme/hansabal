a98ad795d9881001e749d3e72043ccfd235cf6dc
devgogogogo <imleo322@gmail.com>
2025-06-10 19:45:11 +0900

    test code : 리뷰 생성

--- src/test/java/org/example/hansabal/domain/review/service/ReviewServiceMockTest.java
+++ src/test/java/org/example/hansabal/domain/review/service/ReviewServiceMockTest.java
@@ -1,6 +1,8 @@
 package org.example.hansabal.domain.review.service;
 
 import org.example.hansabal.common.jwt.UserAuth;
+import org.example.hansabal.domain.product.entity.Product;
+import org.example.hansabal.domain.product.entity.ProductStatus;
 import org.example.hansabal.domain.product.repository.ProductRepository;
 import org.example.hansabal.domain.review.dto.request.CreateReviewRequest;
 import org.example.hansabal.domain.review.dto.response.CreateReviewResponse;
@@ -9,8 +11,6 @@ import org.example.hansabal.domain.review.repository.ReviewRepository;
 import org.example.hansabal.domain.users.entity.User;
 import org.example.hansabal.domain.users.entity.UserRole;
 import org.example.hansabal.domain.users.repository.UserRepository;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -27,6 +27,8 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class ReviewServiceMockTest {
 
+    //@Mock: ReviewService가 의존하는
+    // ReviewRepository, UserRepository, ProductRepository 등의 Mock 객체를 선언합니다
     @Mock
     private ReviewRepository reviewRepository;
 
@@ -36,42 +38,45 @@ public class ReviewServiceMockTest {
     @Mock
     private ProductRepository productRepository;
 
+    //Test 대상 클래스(ReviewService)의 인스턴스를 생성하며, @Mock으로 선언된 가짜 객체들이 의존성으로 주입됩니다.
     @InjectMocks
     private ReviewService reviewService;
 
-    /**
-     * <a href="https://it-is-mine.tistory.com/3">네이밍 컨벤션 체크</a>
-     */
     @Test
-    @DisplayName("리뷰_생성")
-    void Given_UserAuth_Then_Create_Success() {
-        // given
+    void 리뷰생성() {
         // given 1 : 파라미터 셋팅
         Long productId = 1L;
         UserAuth userAuth = new UserAuth(1L, UserRole.USER);
         CreateReviewRequest request = new CreateReviewRequest("테스트 리뷰", 5);
-
-        // given 2 : 응답 결과값 셋팅
+        Product product = new Product("테스트 제품", 10, ProductStatus.FOR_SALE);
+        // given 2 : 테스트 결과 예상값 셋팅
         Long reviewId = 1L;
         User user = new User("테스트 이메일", "테스트 비밀번호", "테스트 이름", "테스트 닉네임");
 
-        // Mock
-        Review savedReview = Mockito.mock(Review.class);
-        given(savedReview.getId()).willReturn(reviewId);
-        given(savedReview.getUser()).willReturn(user);
-        given(savedReview.getContent()).willReturn(request.getContent());
+        // Mock review (가짜)객체를 생성한다.
+        // given() 메서드를 사용하는 이유 : Mock 객체는 명시적으로 동작을 설정하지 않으면 아무런 동작도 하지 않습니다.
+        Review review = Mockito.mock(Review.class);
+        given(review.getId()).willReturn(reviewId);
+        given(review.getUser()).willReturn(user);
+        given(review.getContent()).willReturn(request.getContent());
 
         // when
+        when(productRepository.findByIdOrElseThrow(anyLong())).thenReturn(product);
         when(userRepository.findByIdOrElseThrow(anyLong())).thenReturn(user);
-        when(reviewRepository.save(any())).thenReturn(savedReview);
+        when(reviewRepository.save(any())).thenReturn(review);
+        CreateReviewResponse response = reviewService.createReview(productId, userAuth, request);
 
         // then
-        CreateReviewResponse response = reviewService.createReview(productId , userAuth, request);
         assertThat(response.getId()).isEqualTo(reviewId);
         assertThat(response.getNickname()).isEqualTo(user.getNickname());
         assertThat(response.getContent()).isEqualTo(request.getContent());
     }
 
-//        assertThrows(BizException.class, () -> reviewService.createReview(productId , userAuth, request));
-//        assertDoesNotThrow(() -> reviewService.createReview(productId , userAuth, request));
+    // 예외 test 할때 사용할 예정참고할 코드 입니다. 추후에 삭제하겠습니다.
+    // assertThrows(BizException .class, () -> reviewService.createReview(productId , userAuth, request));
+    // assertDoesNotThrow(() -> reviewService.createReview(productId , userAuth, request));
+    @Test
+    void 조회() {
+
+    }
 }
