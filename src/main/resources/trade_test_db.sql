0babb8a4b1644f3b27a7ec37db81a8dec72bf8ee
sconers00 <jungnam369@naver.com>
2025-06-13 21:57:31 +0900

    fix : test requests 수정

--- src/main/resources/trade_test_db.sql
+++ src/main/resources/trade_test_db.sql
@@ -3,19 +3,19 @@ DROP TABLE IF EXISTS trade;
 
 
 CREATE TABLE trade (
-                         id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                         title VARCHAR(30) NOT NULL ,
-                         contents TEXT ,
-                         trader BIGINT NOT NULL ,
-                         price BIGINT NOT NULL ,
-                         is_occupied tinyint NOT NULL,
-                         created_at DATETIME(6),
-                         updated_at DATETIME(6),
-                         deleted_at DATETIME(6),
-                         FOREIGN KEY (trader) REFERENCES users(id)
+                       id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                       title VARCHAR(30) NOT NULL ,
+                       contents TEXT ,
+                       user_id BIGINT NOT NULL ,
+                       price BIGINT NOT NULL ,
+                       is_occupied tinyint NOT NULL,
+                       created_at DATETIME(6),
+                       updated_at DATETIME(6),
+                       deleted_at DATETIME(6),
+                       FOREIGN KEY (user_id) REFERENCES users(id)
 ) ENGINE=InnoDB;
 
-INSERT INTO trade (id, title, contents, trader, price,is_occupied,created_at, updated_at, deleted_at)
+INSERT INTO trade (id, title, contents, user_id, price,is_occupied, created_at, updated_at, deleted_at)
 VALUES(1,'test1','testcontents',1,25000,0,now(),now(),null),
       (2,'test2','testcontents',1,15000,1,now(),now(),null),
       (3,'test3','testcontents',2,33000,1,now(),now(),null),
