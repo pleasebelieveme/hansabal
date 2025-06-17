f963e6ea20d7892960bee78522c377745aaf73bc
Toastvillein <okkcba7899@gmail.com>
2025-06-17 17:08:29 +0900

    feat: chat domain and configs

---
+++ src/main/java/org/example/hansabal/common/jwt/JwtHandshakeInterceptor.java
@@ -0,0 +1,81 @@
+package org.example.hansabal.common.jwt;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtHandshakeInterceptor implements HandshakeInterceptor {
+
+	private final JwtUtil jwtUtil;
+
+	/**
+	 * WebSocket 연결 요청이 들어올 때 최초 1회 실행됨
+	 * JWT를 검증하고 SecurityContext에 인증 정보를 등록함
+	 * JwtFilter와 유사한 형태를 띄고 있지만 JwtFilter는 HTTP 요청마다 적용되는 반면,
+	 * HandshakeInterceptor 는 WebSocket 연결 시에만 작동됨
+	 * 적용 위치와 용도, 실행 시점등이 상이하며 HttpServlet chain과 별개로 동작하기 때문에
+	 * 따로 구현해야 함
+	 */
+	@Override
+	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+		Map<String, Object> attributes) throws Exception {
+
+		String token = extractToeknFromRequest(request);
+		if (token == null || !jwtUtil.validateToken(token)){
+			log.warn("WebSocket 인증 실패: 토큰이 유효하지 않음");
+			response.setStatusCode(HttpStatus.UNAUTHORIZED);
+			return false;
+		}
+
+		UserAuth userAuth = jwtUtil.extractUserAuth(token);
+
+		List<GrantedAuthority> authorities = List.of(
+			new SimpleGrantedAuthority("ROLE_" + userAuth.getUserRole().name())
+		);
+
+		Authentication authentication = new UsernamePasswordAuthenticationToken(
+			userAuth,null,authorities
+		);
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		return true;
+	}
+
+	@Override
+	public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+		Exception exception) {
+
+	}
+
+	private String extractToeknFromRequest(ServerHttpRequest request){
+		List<String> headers = request.getHeaders().get("Authorization");
+		if(headers == null || headers.isEmpty()){
+			return null;
+		}
+
+		String bearer = headers.get(0);
+		if(bearer.startsWith("Bearer ")){
+			return bearer.substring(7);
+		}
+
+		return null;
+	}
+}
---
+++ src/main/java/org/example/hansabal/domain/chat/entity/Chat.java
@@ -0,0 +1,42 @@
+package org.example.hansabal.domain.chat.entity;
+
+import java.time.LocalDateTime;
+
+import org.example.hansabal.common.base.BaseEntity;
+import org.example.hansabal.domain.users.entity.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class Chat extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "sender_id",nullable = false)
+	private User sender;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "receiver_id",nullable = false)
+	private User receiver;
+
+	private String content;
+
+	private LocalDateTime sentAt;
+
+	@PrePersist
+	public void prePersist() {
+		this.sentAt = LocalDateTime.now();
+	}
+
+}
