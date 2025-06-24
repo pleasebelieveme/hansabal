e1a29c0834bdaf6d4548049f4b739ea10b91ce36
pleasebelieveme <cain1227@naver.com>
2025-06-24 14:50:40 +0900

    refactor: 어스 테스트 변경 전 코드점검

--- src/main/java/org/example/hansabal/domain/auth/exception/AuthErrorCode.java
+++ src/main/java/org/example/hansabal/domain/auth/exception/AuthErrorCode.java
@@ -9,10 +9,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
-	NOT_FOUND_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "A001", "액세스 토큰이 유효한 형태가 아닙니다."),
-	NOT_FOUND_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "A002","리프레시 토큰이 유효한 형태가 아닙니다."),
+	INVALID_AUTH_HEADER(HttpStatus.UNAUTHORIZED, "A001", "Authorization 헤더 형식이 잘못되었습니다."),
+	INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "A002", "리프레시 토큰이 유효하지 않습니다."),
 	INVALID_REFRESH_TOKEN_SIGNATURE(HttpStatus.UNAUTHORIZED, "A003", "리프레시 토큰 서명이 유효하지 않습니다."),
-	EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "A004", "리프레시 토큰이 만료되었습니다."),
+	EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "A004", "리프레시 토큰이 만료되었습니다. 다시 로그인하세요."),
 	MISMATCHED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "A005", "리프레시 토큰 정보가 일치하지 않습니다."),
 	REUSED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "A006", "이미 사용된 리프레쉬 토큰입니다."),
 	OAUTH2_PROVIDER_NOT_SUPPORTED(HttpStatus.BAD_REQUEST, "A007", "지원하지 않는 OAuth2 제공자입니다."),
