7051c604ca7c883c30e44c5972190bf93b8c0734
sconers00 <jungnam369@naver.com>
2025-06-25 20:18:38 +0900

    refactor : payment
    validation 확인용 메서드 추가
    원본 validation 컨트롤러 메서드 주석처리

--- src/main/java/org/example/hansabal/domain/payment/controller/PaymentController.java
+++ src/main/java/org/example/hansabal/domain/payment/controller/PaymentController.java
@@ -1,5 +1,8 @@
 package org.example.hansabal.domain.payment.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.example.hansabal.domain.payment.dto.request.RequestPay;
 import org.example.hansabal.domain.payment.dto.request.PaymentCallbackRequest;
 import org.example.hansabal.domain.payment.service.PaymentService;
@@ -36,14 +39,27 @@ public class PaymentController {
 		return "payment";
 	}
 
+	// @ResponseBody
+	// @PostMapping("/api/payment")
+	// public ResponseEntity<IamportResponse<Payment>> validationPayment(@RequestBody PaymentCallbackRequest request) {
+	// 	IamportResponse<Payment> iamportResponse = paymentService.paymentByCallback(request);
+	//
+	// 	log.info("결제 응답={}", iamportResponse.getResponse().toString());
+	//
+	// 	return new ResponseEntity<>(iamportResponse, HttpStatus.OK);
+	// }
+
 	@ResponseBody
-	@PostMapping("/api/payment")
-	public ResponseEntity<IamportResponse<Payment>> validationPayment(@RequestBody PaymentCallbackRequest request) {
+	@PostMapping("/api/payment")//확인용
+	public ResponseEntity<Map<String, String>> validationPaymentV2(@RequestBody PaymentCallbackRequest request) {
 		IamportResponse<Payment> iamportResponse = paymentService.paymentByCallback(request);
 
 		log.info("결제 응답={}", iamportResponse.getResponse().toString());
+		Map<String, String> response = new HashMap<>();
+		response.put("status", "success");
+		response.put("message", "결제 완료");
 
-		return new ResponseEntity<>(iamportResponse, HttpStatus.OK);
+		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 
 	@GetMapping("/api/success-payment")
