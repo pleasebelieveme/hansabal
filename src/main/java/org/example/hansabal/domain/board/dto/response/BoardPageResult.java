521e6007666241a3f012c25b809433c6e48040a0
남유리 <skadbfl11225@gmail.com>
2025-07-04 15:48:49 +0900

    fix : Board 캐싱 작업 수정

--- src/main/java/org/example/hansabal/domain/board/dto/response/BoardPageResult.java
+++ src/main/java/org/example/hansabal/domain/board/dto/response/BoardPageResult.java
@@ -2,24 +2,37 @@ package org.example.hansabal.domain.board.dto.response;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.domain.Page;
 
-public record BoardPageResult(
-	List<BoardSimpleResponse> contents,
-	int page,
-	int size,
-	long totalElements
-) {
-	public boolean isEmpty(){
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS,include = JsonTypeInfo.As.PROPERTY,property = "@class")
+public class BoardPageResult{
+
+	private List<BoardSimpleResponse> contents;
+	private int page;
+	private int size;
+	private long totalElements;
+
+	public boolean isEmpty() {
 		return contents == null || contents.isEmpty();
 	}
 
-	public static  BoardPageResult of(Page<BoardSimpleResponse> result) {
+	public static BoardPageResult of(Page<BoardSimpleResponse> result) {
 		return new BoardPageResult(
-			result.getContent(),
-			result.getNumber() + 1,
-			result.getSize(),
-			result.getTotalElements()
+				result.getContent(),
+				result.getNumber() + 1,
+				result.getSize(),
+				result.getTotalElements()
 		);
 	}
 }
--- src/main/java/org/example/hansabal/domain/board/dto/response/BoardSimpleResponse.java
+++ src/main/java/org/example/hansabal/domain/board/dto/response/BoardSimpleResponse.java
@@ -1,18 +1,32 @@
 package org.example.hansabal.domain.board.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.example.hansabal.domain.board.entity.BoardCategory;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS,include = JsonTypeInfo.As.PROPERTY,property = "@class")
 public class BoardSimpleResponse {
 
-    private final String writer;
-    private final BoardCategory category;
-    private final String title;
-    private final Integer viewCount;
-    private final Integer dibCount;
+    private String writer;
+    private BoardCategory category;
+    private String title;
+    private int viewCount;
+    private int dibCount;
+
+    public BoardSimpleResponse(String writer, BoardCategory category, String title, Integer viewCount, Integer dibCount) {
+        this.writer = writer;
+        this.category = category;
+        this.title = title;
+        this.viewCount = viewCount;
+        this.dibCount = dibCount;
+    }
 }
